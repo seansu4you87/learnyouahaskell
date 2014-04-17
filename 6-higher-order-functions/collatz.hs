8c826605e710ff353c7592f179d8cd9b5642a72f
Sean Yu <seansu4you87@gmail.com>
2014-04-17 12:25:03 -0700

    Chapter 6

---
+++ 6-higher-order-functions/collatz.hs
@@ -0,0 +1,8 @@
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n:chain (n `div` 2)
+  | odd n  = n:chain (n*3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
---
+++ 6-higher-order-functions/higher_order.hs
@@ -0,0 +1,31 @@
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f y x = f x y
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+  | p x       = x : filter' p xs
+  | otherwise = filter' p xs
+
+quicksortFilter :: (Ord a) => [a] -> [a]
+quicksortFilter [] = []
+quicksortFilter (x:xs) =
+  let smallerSorted = quicksortFilter (filter (<=x) xs)
+      biggerSorted  = quicksortFilter (filter (>x) xs)
+  in  smallerSorted ++ [x] ++ biggerSorted
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000,99999..])
+  where p x = x `mod` 3829 == 0
